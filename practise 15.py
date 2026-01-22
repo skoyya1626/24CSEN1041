e944df1dadbca84c4c2d034d7d67591f63b48f05
skoyya1626 <skoyya@student.gitam.edu>
2026-01-22 14:30:39 +0530

    precedence-associativity
    This script performs various arithmetic operations and prints the results.

---
+++ practise 15.py
@@ -0,0 +1,17 @@
+a = 500
+b = 20
+
+c = a + b * 3
+print(f"Value of c = {c}")
+
+c = a // b * 4   # use // for integer division like C
+print(f"Value of c = {c}")
+
+c = 3 % b * a
+print(f"Value of c = {c}")
+output:-
+Value of c = 560
+Value of c = 100
+Value of c = 1500
+
+=== Code Execution Successful ===
