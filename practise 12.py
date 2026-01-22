440f73f14021497984206d5556382e07e5f1760b
skoyya1626 <skoyya@student.gitam.edu>
2026-01-22 14:22:08 +0530

    format specifiers

---
+++ practise 12.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number:"))
+
+print(f"Decimal representation: {a}")
+print(f"Octal representation: {oct(a)}")
+print(f"Hexadecimal representation: {hex(a)}")
+
+output:-
+Enter a number:14
+Decimal representation: 14
+Octal representation: 0o16
+Hexadecimal representation: 0xe
