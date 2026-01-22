a70a97144c6da9e9616278745b2f90f5b9040d7a
skoyya1626 <skoyya@student.gitam.edu>
2026-01-22 14:41:45 +0530

    Implement factorial calculation using recursion

---
+++ practise 17.py
@@ -0,0 +1,18 @@
+def recursive_factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * recursive_factorial(n - 1) 
+
+number = int(input("Enter a non-negative number: "))
+
+factorial_number = 1
+
+if number < 0:
+    print("Entered a negative number. Factorial cannot be determined")
+else:
+    for i in range(1, number + 1):   
+        factorial_number *= i
+    print(f"Factorial of {number} is {factorial_number}")
+
+    print(f"\nUsing recursive method Factorial of {number} is {recursive_factorial(number)}")
