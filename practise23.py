1e89c83d5d65c770e1e09802a853d452d841c4dd
skoyya1626 <skoyya@student.gitam.edu>
2026-02-04 11:22:55 +0530

    string slicing

--- practise23.py
+++ practise23.py
@@ -2,13 +2,13 @@
 text = "Programming"
 print("text =", text)
 
-print("text[0:4]   =", text[0:4])     # Prog
-print("text[6:-3]   =", text[6:-3])     # Prog
-print("text[3:]    =", text[3:])      # gramming
-print("text[:5]    =", text[:5])      # Progr
-print("text[-4:]   =", text[-4:])     # ming
-print("text[::3]   =", text[::3])     # Pormig
-print("text[::-2]  =", text[::-2])    # reverse
+print("text[0:4]   =", text[0:4])   
+print("text[6:-3]   =", text[6:-3])   
+print("text[3:]    =", text[3:])     
+print("text[:5]    =", text[:5])     
+print("text[-4:]   =", text[-4:])     
+print("text[::3]   =", text[::3])    
+print("text[::-2]  =", text[::-2])  
 print()
 output:-
 text = Programming
