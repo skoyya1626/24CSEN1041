aa3d6480f81c3cfaae8a77ef9e247883bc0ef2fc
skoyya1626 <skoyya@student.gitam.edu>
2026-02-04 10:37:36 +0530

    string slicing
    Added output examples for string slicing in Python.

---
+++ practise23.py
@@ -0,0 +1,24 @@
+# String slicing
+text = "Programming"
+print("text =", text)
+
+print("text[0:4]   =", text[0:4])     # Prog
+print("text[6:-3]   =", text[6:-3])     # Prog
+print("text[3:]    =", text[3:])      # gramming
+print("text[:5]    =", text[:5])      # Progr
+print("text[-4:]   =", text[-4:])     # ming
+print("text[::3]   =", text[::3])     # Pormig
+print("text[::-2]  =", text[::-2])    # reverse
+print()
+output:-
+text = Programming
+text[0:4]   = Prog
+text[6:-3]   = mm
+text[3:]    = gramming
+text[:5]    = Progr
+text[-4:]   = ming
+text[::3]   = Pgmn
+text[::-2]  = gimroP
+
+
+=== Code Execution Successful ===
